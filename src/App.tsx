ac4be8774e385109bad3cd3fd31a3745852f0c3d
THONWELLING <thonwellingdani13@gmail.com>
2022-01-19 01:42:16 -0300

    cria primeiro componente

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,24 @@
 import * as C from './App.styles'
-
+import { useState } from 'react'
+import { Item } from './types/item'
+import { ListItem } from './components/ListItem'
 
 const App = () => {
+  const [list, setList] = useState<Item[]>([
+    {id: 1, name: 'buy an additional monitor to study', done: false},
+    {id: 2, name: 'Exercise React more to be able to get node to backend', done: false},
+  ])
   return (
     <C.Container>
       <C.Area>
         <C.Header>Task List!!</C.Header>
+
+        {/* add new task area */}
+
+        {/*  task list */}
+        {list.map((item, index) => (
+          <ListItem key={index} item={item}/>
+        ))}
       </C.Area>
     </C.Container>
   )
