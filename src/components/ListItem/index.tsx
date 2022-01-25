481f685e46d5652e2c3f69ef1e8264066307efc0
THONWELLING <thonwellingdani13@gmail.com>
2022-01-24 23:09:10 -0300

    corrige bug não estava passando o traço en cima da terefa concluida

--- src/components/ListItem/index.tsx
+++ src/components/ListItem/index.tsx
@@ -10,14 +10,14 @@ type Props ={
 export const ListItem = ({ item , onChange}: Props) => {
   const [isChecked, setIsChecked] =useState(item.done)
   return (
-    <C.Container done={isChecked}>
+    <C.Container done={item.done}>
       <input 
         type='checkbox' 
         checked={item.done}
         onChange={e => onChange(item.id, e.target.checked)}
       />
       <label>{item.name}</label>
-      {/* para teste se está funcionando corretamente adicione o seguinte código - {item.done.toString()} ao label e click no checkbox e veja a mudança*/}
+
       </C.Container>
   )
 }
